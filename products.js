806ec434a588fcc06ec943a53c2281af2c067632
Sharp1991 <shaphrang.syiem@gmail.com>
2025-08-17 16:35:24 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,13 @@
+// js/products.js
 
+const products = [];
+
+for (let i = 1; i <= 50; i++) {
+  products.push({
+    name: `Product ${i}`,
+    description: `Description for Product ${i}.`,
+    price: `₹${100 + i*10}`,
+    image: `images/product${i}.jpg`,
+    whatsapp: `https://wa.me/1234567890?text=I%20want%20to%20buy%20Product%20${i}`
+  });
+}
